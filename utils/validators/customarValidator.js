897b9a245938b87f3f34b8e31f40c820873abd3f
Muhammed <muhammedelhalil0@gmail.com>
2023-08-25 17:20:34 +0300

    customar

--- utils/validators/customarValidator.js
+++ utils/validators/customarValidator.js
@@ -1,26 +1,72 @@
 const { check, body, param } = require("express-validator");
 const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+const Customar =require('../../models/customarModel');
 
-
+//validator for create one customar
 exports.createCustomarVlaidator = [
-    check('name')
-        .notEmpty().withMessage('The name can not be empty')
-        .isLength({min:3}).withMessage('The name is too short')
-        .isLength({max:30}).withMessage('The name is too long'),
-    check('phoneNumber')
-        .optional()
-        .isMobilePhone(["tr-TR"]).withMessage("Invalid phone number Only accepted turkey phone numbers"),
-    check('email')
-        .notEmpty().withMessage('The email can not be empty')
-        .isEmail().withMessage('Invalid email address')
+  check("name")
+    .notEmpty()
+    .withMessage("The name can not be empty")
+    .isLength({ min: 3 })
+    .withMessage("The name is too short")
+    .isLength({ max: 30 })
+    .withMessage("The name is too long"),
+  check("phoneNumber")
+    .optional()
+    .isMobilePhone(["tr-TR"])
+    .withMessage("Invalid phone number Only accepted turkey phone numbers"),
+  check("email")
+    .notEmpty()
+    .withMessage("The email can not be empty")
+    .isEmail()
+    .withMessage("Invalid email address")
 
-        .custom((val)=>findOne({email:val}).then((customar)=>{
-                if(customar){
-                    return Promise.reject(new Error ('Email already in customar'));
+    .custom((val) =>
+    Customar.findOne({ email: val }).then((customar) => {
+        if (customar) {
+          return Promise.reject(new Error("Email already in customar"));
+        }
+      })
+    ),
+
+  validatorMiddleware,
+];
+
+//Validator for update an customar
+exports.updataCustomarVlaidator = [
+    param("id").isMongoId().withMessage("Invalid customar id"),
+    check("name")
+        .notEmpty()
+        .withMessage("The name can not be empty")
+        .isLength({ min: 3 })
+        .withMessage("The name is too short")
+        .isLength({ max: 30 })
+        .withMessage("The name is too long"),
+    check("phoneNumber")
+        .optional()
+        .isMobilePhone(["tr-TR"])
+        .withMessage("Invalid phone number Only accepted turkey phone numbers"),
+    check("email")
+        .notEmpty()
+        .withMessage("The email can not be empty")
+        .isEmail()
+        .withMessage("Invalid email address")
+        .custom((val) => Customar.findOne({ email: val }).then((customar) => {
+                if (customar) {
+                    return Promise.reject(new Error("Email already in customar"));
                 }
-            })
+            }) 
         ),
 
     validatorMiddleware,
+];
 
+exports.getCustomarVlaidator = [
+    check('id').isMongoId().withMessage("Invalid customar id"),
+    validatorMiddleware,
+];
+
+exports.deleteCustomarVlaidator = [
+    check('id').isMongoId().withMessage("Invalid customar id"),
+    validatorMiddleware,
 ];
