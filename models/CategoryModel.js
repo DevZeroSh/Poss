0872c6439160117ae6cccd0cf0566a0177eaa3cc
MohammadShahror <mohmmadshr9@gmail.com>
2023-08-30 16:57:30 +0300

    Category

--- models/CategoryModel.js
+++ models/CategoryModel.js
@@ -1,25 +1,10 @@
 const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: [true, "Category must be unique"],
-  },
-  slug: {
-    type: String,
-    lowercase: true,
-  },
-  description: {
-    type: String,
-    minlength: [9, "too short brand description"],
-    maxlength: [100, "too long brand description"],
-  },
-  parentCategory: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Category",
-  },
+  name: String,
+  parentCategory: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
 });
 
-const categoryModel = mongoose.model("Category", categorySchema);
-module.exports = categoryModel;
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
