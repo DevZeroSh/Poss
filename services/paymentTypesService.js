833029182c5f12ae655e055c42f25a28f4c77852
Muhammed <muhammedelhalil0@gmail.com>
2023-10-04 13:36:51 +0300

    payment types

--- services/paymentTypesService.js
+++ services/paymentTypesService.js
@@ -8,5 +8,68 @@ const ApiError = require("../utils/apiError");
 exports.getPaymentTypes = asyncHandler(async (req, res) => {
     const paymentType = await PaymentTypes.find({});
     res.status(200).json({ status: "true", data: paymentType });
-  });
+});
+
+//@desc Create payment type
+//route Post /api/paymenttype
+//@access Private
+exports.createPaymentType = asyncHandler(async (req, res, next) => {
+    const paymentType = await PaymentTypes.create(req.body);
+
+    res.status(200).json({
+        status: "true",
+        message: "Payment Inserted",
+        data: paymentType,
+    });
+});
+
+//@desc Get One payment type
+//route Post /api/paymenttype
+//@access Private
+exports.getOnePaymentType = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const paymentType = await PaymentTypes.findById(id);
+    if (!paymentType) {
+        return next(
+            new ApiError(`There is no payment type with this id ${id}`, 404)
+        );
+    } else {
+        res.status(200).json({ status: "true", data: paymentType });
+    }
+});
+
+//Update one Payment type
+//route Put /api/paymenttype
+//@access Private
+exports.updataPaymentType = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const paymentType = await PaymentTypes.findByIdAndUpdate(id, req.body, {
+        new: true,
+    });
+
+    if (!paymentType) {
+        return next(
+            new ApiError(`There is no payment type with this id ${id}`, 404)
+        );
+    } else {
+        res.status(200).json({
+            status: "true",
+            message: "Payment type updated",
+            data: paymentType,
+        });
+    }
+});
+
+// @desc Delete specific payment type
+// @rout Delete /api/paymenttype/:id
+// @access priveta
+exports.deleteOnePaymentType = asyncHandler(async (req, res, next) => {
   
+    const { id } = req.params;
+    const paymentType = await PaymentTypes.findByIdAndDelete(id);
+
+    if (!paymentType) {
+        return next(new ApiError(`No payment type for this id ${id}`, 404));
+    }
+    res.status(200).json({ status: "true", message: "Payment type Deleted" });
+});
