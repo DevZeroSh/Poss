e998d3d652caabcbc0031c3fb1284a5a89a2f2d6
Muhammed <muhammedelhalil0@gmail.com>
2023-11-06 15:02:53 +0300

    .

--- server.js
+++ server.js
@@ -39,11 +39,11 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "uploads")));
 
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-  console.log(`mode: ${process.env.NODE_ENV}`);
+    app.use(morgan("dev"));
+    console.log(`mode: ${process.env.NODE_ENV}`);
 }
 
-dbContacion()
+dbContacion();
 
 //Routes' links
 app.use("/api/product", productRout);
@@ -69,19 +69,18 @@ app.use("/api/financialfunds", financialFundsRoute);
 app.use("/api/expenses", expensesRoute);
 app.use("/api/productinvoices", productInvoicesRout);
 
-
 //Global error handling middleware for express
 app.use(globalError);
 
 const PORT = process.env.PORT || 4000;
 const server = app.listen(PORT, () => {
-  console.log(`app running on port ${PORT}`);
+    console.log(`app running on port ${PORT}`);
 });
 
 process.on("unhandledRejection", (err) => {
-  console.error(`unhandledRejection Errors:${err.name} | ${err.message}`);
-  server.close(() => {
-    console.error(`Shutting down....`);
-    process.exit(1);
-  });
+    console.error(`unhandledRejection Errors:${err.name} | ${err.message}`);
+    server.close(() => {
+        console.error(`Shutting down....`);
+        process.exit(1);
+    });
 });
