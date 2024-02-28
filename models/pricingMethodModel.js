96e8e20b91dd94b2a439e57d939de155329e68e2
DevZeroSh <m.shahrour.work@gmail.com>
2024-02-28 15:37:19 +0300

    fixed bugs

--- models/pricingMethodModel.js
+++ models/pricingMethodModel.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
 
 const pricingMethodSchema = new mongoose.Schema({
-    selectedCategory: {
-        type: mongoose.Schema.ObjectId,
-        ref: "Category",
-        required: true,
-    },
+  selectedCategory: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Category",
+    unique: true, // Add unique constraint
+    required: true,
+  },
 
-    percentageIncrease: {
-        type: Number,
-        required: true,
-        min: 0,
-        max: 100,
-    },
+  percentageIncrease: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 100,
+  },
 });
 
 module.exports = pricingMethodSchema;
--- services/pricingMethodService.js
+++ services/pricingMethodService.js
@@ -8,19 +8,22 @@ const categorySchema = require("../models/CategoryModel");
 //@route GET  /api/pricingmethod
 //@accsess Private
 exports.getPricingMethods = asyncHandler(async (req, res) => {
-    const dbName = req.query.databaseName;
-    const db = mongoose.connection.useDb(dbName);
-    const PricingMethodModel = db.model("PricingMethod", pricingMethodSchema);
-    db.model("Category", categorySchema);
+  const dbName = req.query.databaseName;
+  const db = mongoose.connection.useDb(dbName);
+  const PricingMethodModel = db.model("PricingMethod", pricingMethodSchema);
+  db.model("Category", categorySchema);
 
-    const pricingMethod = await PricingMethodModel.find().populate({ path: "selectedCategory" });
-    res.status(200).json({ status: "true", data: pricingMethod });
+  const pricingMethod = await PricingMethodModel.find().populate({
+    path: "selectedCategory",
+  });
+  res.status(200).json({ status: "true", data: pricingMethod });
 });
 
 //@desc Create a pricing Method
 //@route POST /api/pricingmethod
 //accsess private
-exports.createPricingMethod = asyncHandler(async (req, res) => {
+exports.createPricingMethod = asyncHandler(async (req, res, next) => {
+  try {
     const catId = new mongoose.Types.ObjectId(req.body.selectedCategory);
     const dbName = req.query.databaseName;
     const db = mongoose.connection.useDb(dbName);
@@ -29,86 +32,109 @@ exports.createPricingMethod = asyncHandler(async (req, res) => {
 
     const category = await categoryModel.findById(catId);
 
-    //check if the category has parent or not
+    // Check if the category has a parent or not
     if (category.parentCategory) {
-        //Yes
-        //Check if the catId exist in Pricing Method table
-        const pricingMethod = await PricingMethodModel.findOne({ selectedCategory: catId });
-        if (pricingMethod) {
-            //Yes,Just update itself
-            pricingMethod.percentageIncrease = req.body.percentageIncrease;
-            await pricingMethod.save();
-            res.status(200).json({ status: "true", message: "Updated", data: pricingMethod });
-        } else {
-            //No, Just add it
-            const addPricingMethod = await PricingMethodModel.create(req.body);
-            res.status(200).json({ status: "true", message: "Inserted", data: addPricingMethod });
-        }
+      // Yes
+      // Check if the catId exists in Pricing Method table
+      const pricingMethod = await PricingMethodModel.findOne({
+        selectedCategory: catId,
+      });
+      if (pricingMethod) {
+        // Yes, just update itself
+        pricingMethod.percentageIncrease = req.body.percentageIncrease;
+        await pricingMethod.save();
+        res
+          .status(200)
+          .json({ status: "true", message: "Updated", data: pricingMethod });
+      } else {
+        // No, just add it
+        const addPricingMethod = await PricingMethodModel.create(req.body);
+        res.status(200).json({
+          status: "true",
+          message: "Inserted",
+          data: addPricingMethod,
+        });
+      }
     } else {
-        //No
-        //Check if the catId exist in Pricing Method table
-        const pricingMethod = await PricingMethodModel.findOne({ selectedCategory: catId });
-        if (pricingMethod) {
-            //Edit to all children of category
-            const editCategoryAndChildren = async (categoryId) => {
-                const theCat = await categoryModel.findById(categoryId);
-                await PricingMethodModel.findOneAndUpdate(
-                    { selectedCategory: theCat._id },
-                    { $set: { percentageIncrease: req.body.percentageIncrease } },
-                    { new: true, upsert: true }
-                );
+      // No
+      // Check if the catId exists in Pricing Method table
+      const pricingMethod = await PricingMethodModel.findOne({
+        selectedCategory: catId,
+      });
+      if (pricingMethod) {
+        // Edit to all children of the category
+        const editCategoryAndChildren = async (categoryId) => {
+          const theCat = await categoryModel.findById(categoryId);
+          await PricingMethodModel.findOneAndUpdate(
+            { selectedCategory: theCat._id },
+            { $set: { percentageIncrease: req.body.percentageIncrease } },
+            { new: true, upsert: true }
+          );
 
-                const children = await categoryModel.find({ parentCategory: theCat._id });
-                for (const child of children) {
-                    await editCategoryAndChildren(child._id);
-                }
-            };
+          const children = await categoryModel.find({
+            parentCategory: theCat._id,
+          });
+          for (const child of children) {
+            await editCategoryAndChildren(child._id);
+          }
+        };
 
-            editCategoryAndChildren(catId)
-                .then(() => {
-                    res.status(200).json({ status: "true", message: "Updated" });
-                })
-                .catch((error) => {
-                    return next(new ApiError(`Error updating PricingMethodModel documents`, 404));
-                });
-        } else {
-            // await PricingMethodModel.create({ selectedCategory: catId, percentageIncrease: req.body.percentageIncrease });
-
-            const addCategoryAndChildren = async (categoryId) => {
-                const theCategory = await categoryModel.findById(categoryId);
-
-                await PricingMethodModel.create({
-                    selectedCategory: theCategory._id,
-                    percentageIncrease: req.body.percentageIncrease,
-                });
-
-                const children = await categoryModel.find({ parentCategory: theCategory._id });
-                for (const child of children) {
-                    await addCategoryAndChildren(child._id);
-                }
-            };
-
-            addCategoryAndChildren(catId)
-                .then(() => {
-                    res.status(200).json({ status: "true", message: "Inserted" });
-                })
-                .catch((error) => {
-                    return next(new ApiError(`Error creating PricingMethodModel documents`, 404));
-                });
-        }
+        await editCategoryAndChildren(catId);
+        res.status(200).json({ status: "true", message: "Updated" });
+      } else {
+        const addCategoryAndChildren = async (categoryId) => {
+          try {
+            const theCategory = await categoryModel.findById(categoryId);
+            const existingPricingMethod = await PricingMethodModel.findOne({
+              selectedCategory: theCategory._id,
+            });
+      
+            if (!existingPricingMethod) {
+              await PricingMethodModel.create({
+                selectedCategory: theCategory._id,
+                percentageIncrease: req.body.percentageIncrease,
+              });
+            } else {
+              console.log("Skipping due to existing document");
+            }
+      
+            const children = await categoryModel.find({
+              parentCategory: theCategory._id,
+            });
+      
+            for (const child of children) {
+              await addCategoryAndChildren(child._id);
+            }
+          } catch (error) {
+            // Handle the specific error here (if needed)
+            console.error(`Error in addCategoryAndChildren: ${error.message}`);
+          }
+        };
+      
+        await addCategoryAndChildren(catId);
+        res.status(200).json({ status: "true", message: "Inserted" });
+      }
+      
     }
+  } catch (error) {
+    return next(
+      new ApiError(`Error in createPricingMethod: ${error.message}`, 500)
+    );
+  }
 });
 
 //@desc get specific category pricing method for category
 //@route GET /api/pricingmethod/:id
 //accsess private
 exports.getSpecificCategoryPricing = asyncHandler(async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const dbName = req.query.databaseName;
-    const db = mongoose.connection.useDb(dbName);
-    const PricingMethodModel = db.model("PricingMethod", pricingMethodSchema);
+  const dbName = req.query.databaseName;
+  const db = mongoose.connection.useDb(dbName);
+  const PricingMethodModel = db.model("PricingMethod", pricingMethodSchema);
 
-    const specificCategory = await PricingMethodModel.findOne({ selectedCategory: id });
-    res.status(200).json({ status: "true", data: specificCategory });
+  const specificCategory = await PricingMethodModel.findOne({
+    selectedCategory: id,
+  });
+  res.status(200).json({ status: "true", data: specificCategory });
 });
