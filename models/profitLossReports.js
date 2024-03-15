748aba748e9490f084742e732fefe34e29708767
Abdulrahman <byjyta@gmail.com>
2024-03-15 14:40:03 +0300

    profitloss model updated

--- models/profitLossReports.js
+++ models/profitLossReports.js
@@ -1,37 +1,41 @@
 const mongoose = require("mongoose");
 
 const ProfitLossReportsSchema = new mongoose.Schema(
-    {
-        year: {
-            type: String,
-            require: [true, "Year missing"],
-        },
-        month: {
-            type: String,
-            require: [true, "Month missing"],
-        },
-        totalSellingPrice: {
-            type: Number,
-            require: [true, "Selling price missing"],
-        },
-        totalSellingCost: {
-            type: Number,
-            require: [true, "Selling cost missing"],
-        },
-        totalExpenses: {
-            type: Number,
-            require: [true, "Expenses missing"],
-        },
-        profit: {
-            type: Number,
-            require: [true, "Profit missing"],
-        },
-        netProfit: {
-            type: Number,
-            require: [true, "Net profit missing"],
-        },
+  {
+    year: {
+      type: String,
+      require: [true, "Year missing"],
     },
-    { timestamps: true }
+    month: {
+      type: String,
+      require: [true, "Month missing"],
+    },
+    totalSellingPrice: {
+      type: Number,
+      require: [true, "Selling price missing"],
+    },
+    totalReturns: {
+      type: Number,
+      require: [true, "Total returns missing"],
+    },
+    totalSellingCost: {
+      type: Number,
+      require: [true, "Selling cost missing"],
+    },
+    totalExpenses: {
+      type: Number,
+      require: [true, "Expenses missing"],
+    },
+    profit: {
+      type: Number,
+      require: [true, "Profit missing"],
+    },
+    netProfit: {
+      type: Number,
+      require: [true, "Net profit missing"],
+    },
+  },
+  { timestamps: true }
 );
 
 module.exports = ProfitLossReportsSchema;
