7c0c34bddbb8dc3518aa2a373439a656127585ff
Abdulrahman <byjyta@gmail.com>
2024-04-24 17:51:53 +0300

    Added funds population to the financial funds reports

--- services/reportsFinancialFundsService.js
+++ services/reportsFinancialFundsService.js
@@ -2,19 +2,23 @@ const mongoose = require("mongoose");
 const asyncHandler = require("express-async-handler");
 const ApiError = require("../utils/apiError");
 const reportsFinancialFundsSchema = require("../models/reportsFinancialFunds");
+const financialFundsSchema = require("../models/financialFundsModel");
 
 //get all financial funds reports
 exports.getReportsFinancialFunds = asyncHandler(async (req, res, next) => {
   const dbName = req.query.databaseName;
   const db = mongoose.connection.useDb(dbName);
-  const ReportsFinancialFundsModel = db.model(
-    "ReportsFinancialFunds",
-    reportsFinancialFundsSchema
-  );
+  db.model("FinancialFunds", financialFundsSchema);
+  const ReportsFinancialFundsModel = db.model("ReportsFinancialFunds", reportsFinancialFundsSchema);
 
   const financialFundReports = await ReportsFinancialFundsModel.find({
     archives: { $ne: true },
-  }).sort({ createdAt: -1 });
+  })
+    .sort({ createdAt: -1 })
+    .populate({
+      path: "financialFundId",
+      select: "fundName",
+    });
   res.status(200).json({ status: "true", data: financialFundReports });
 });
 
@@ -24,10 +28,7 @@ exports.getSpecificReports = asyncHandler(async (req, res, next) => {
   const dbName = req.query.databaseName;
 
   const db = mongoose.connection.useDb(dbName);
-  const ReportsFinancialFundsModel = db.model(
-    "ReportsFinancialFunds",
-    reportsFinancialFundsSchema
-  );
+  const ReportsFinancialFundsModel = db.model("ReportsFinancialFunds", reportsFinancialFundsSchema);
 
   const financialReports = await ReportsFinancialFundsModel.find({
     financialFundId: id,
