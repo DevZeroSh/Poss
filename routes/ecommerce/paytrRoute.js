ad12541629d5f714a631c0853be13f46aa27c107
Mohammed Shahrour <142575363+DevZeroSh@users.noreply.github.com>
2024-07-08 13:10:33 +0300

    Update paytrRoute.js

--- routes/ecommerce/paytrRoute.js
+++ routes/ecommerce/paytrRoute.js
@@ -108,7 +108,7 @@ const callback = req.body;
   console.log(callback);
   console.log("-------callback end-------\n");
    console.log("\n-------callback-------");
-console.log(req);
+console.log(res);
   // Construct the hash
   const paytr_token =
     callback.merchant_oid +
