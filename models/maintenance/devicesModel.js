5ad3ec828aa184112befc8da7d43b9f7c15495c0
DevZeroSh <m.shahrour.work@gmail.com>
2024-10-04 17:49:26 +0300

    add brand

--- models/maintenance/devicesModel.js
+++ models/maintenance/devicesModel.js
@@ -5,6 +5,7 @@ const devicesSchema = new mongoose.Schema(
     userId: { type: mongoose.Schema.ObjectId, ref: "manitUser" },
     serialNumber: String,
     deviceType: String,
+    deviceBrand: String,
     deviceModel: String,
     counter: String,
   },
