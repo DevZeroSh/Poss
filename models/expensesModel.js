0652144ac8eeedc1867528fa8041a6d39206abff
DevZeroSh <m.shahrour.work@gmail.com>
2024-10-15 16:09:38 +0300

    fixed bugs

--- models/expensesModel.js
+++ models/expensesModel.js
@@ -1,43 +1,44 @@
 const mongoose = require("mongoose");
 
-const expensesSchema = new mongoose.Schema({
-  expenseName: String,
-  expenseDate: Date,
-  expenseCategoryId: String,
-  expenseCategory: String,
-  supplierId: String,
-  supplierName: String,
-  invoiceCurrencyTotal: Number,
-  currencyId: String,
-  currencyCode: String,
-  currencyExchangeRate: String,
-  Tax: String,
-  TaxId: String,
-  MainCurrencyTotal: Number,
-  paymentStatus: { type: String, default: "Pending" },
-  paymentDate: Date,
-  finincalFund: String,
-  employeeID: String,
-  employeeName: String,
-  expenseClarification: Number,
-  expenseFile: String,
-  receiptNumber: String,
-  totalRemainderMainCurrency: { type: Number, default: 0 },
-  totalRemainder: { type: Number, default: 0 },
-  counter: { type: String, default: 0, unique: true },
-});
+const expensesSchema = new mongoose.Schema(
+  {
+    expenseName: String,
+    expenseDate: Date,
+    expenseCategoryId: String,
+    expenseCategory: String,
+    supplierId: String,
+    supplierName: String,
+    invoiceCurrencyTotal: Number,
+    currencyId: String,
+    currencyCode: String,
+    currencyExchangeRate: String,
+    Tax: String,
+    TaxId: String,
+    MainCurrencyTotal: Number,
+    paymentStatus: { type: String, default: "Pending" },
+    paymentDate: Date,
+    finincalFund: String,
+    employeeID: String,
+    employeeName: String,
+    expenseClarification: Number,
+    expenseFile: String,
+    receiptNumber: String,
+    totalRemainderMainCurrency: { type: Number, default: 0 },
+    totalRemainder: { type: Number, default: 0 },
+    counter: { type: String, default: 0, unique: true },
+  },
+  { timestamps: true }
+);
 
 const setFileURL = (doc) => {
-  if (doc.expenseFile && doc.expenseFile.length > 0) {
-    doc.expenseFile = `${process.env.BASE_URL}/expenses/${file}`;
+  if (doc.expenseFile) {
+    doc.expenseFile = `${process.env.BASE_URL}/expenses/${doc.expenseFile}`;
   }
 };
 
 //When findOne, findAll and update
 expensesSchema.post("init", (doc) => {
-  if (doc?.expenseFile) {
-    setFileURL(doc?.expenseFile);
-  }
+  setFileURL(doc);
 });
 
 //When createOne
