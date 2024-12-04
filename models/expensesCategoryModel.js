154acc40ff230f74be966431e804a033f0dfdd20
smartinbSolutions <160218438+smartinbSolutions@users.noreply.github.com>
2024-12-04 17:53:45 +0300

    expenceCategory

--- models/expensesCategoryModel.js
+++ models/expensesCategoryModel.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
 const expensesCategorySchama = new mongoose.Schema({
-    expenseCategoryName: {
-        type: String,
-        require: true,
-        unique: [true, "Expense Category Name must be unique"]
-    },
-    expenseCategoryDescription: String,
+  expenseCategoryName: {
+    type: String,
+    require: true,
+    unique: [true, "Expense Category Name must be unique"],
+  },
+  expenseCategoryDescription: String,
+  linkedAccount: {
+    type: String,
+    require: true,
+  },
 });
 
 module.exports = expensesCategorySchama;
